cc5c7046814db0993e0422a08ffcadf6fd0d170d
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2024-08-10 23:26:12 +0400

    sending frame data properly

--- static/js/index.js
+++ static/js/index.js
@@ -3,10 +3,20 @@ async function getMedia() {
         const stream = await navigator.mediaDevices.getUserMedia({
             video: true
         });
-        const video = document.querySelector("video");
+        const video = document.querySelector("#video");
         video.srcObject = stream;
+
+        await new Promise((resolve) => {
+            video.addEventListener("loadedmetadata", () => {
+                resolve();
+            });
+        });
+
+        console.log(video.videoWidth, "x", video.videoHeight);
+
     } catch (e) {
         console.error("Cannot get webcam input:", e);
+        throw(e);
     }
 }
 
@@ -17,36 +27,39 @@ async function captureFrame() {
 
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
+    console.log(canvas.width, "x", canvas.height)
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.heigth);
-
-    return new Promise((resolve) => {
-        canvas.toBlob((blob) => {
-            blob.arrayBuffer().then((buffer) => {
-                resolve(buffer);
-            });
+    return new Promise((resolve, reject) => {
+        canvas.toBlob(blob => {
+            if (blob) {
+                blob.arrayBuffer().then(buffer => {
+                    resolve(buffer);
+                }).catch(reject);
+            } else {
+                reject(new Error("Failed to create blob from canvas"));
+            }
         }, "image/png");
     });
 }
 
-function sendData(imgData) {
-    const ws = new WebSocket("ws://127.0.0.1:8080/ws");
-
-    ws.addEventListener("open", () => {
+function sendData(ws, imgData) {
+    if (ws.readyState !== WebSocket.CLOSED) {
+        console.log(imgData);
         ws.send(imgData);
-    });
+    } else {
+        console.error("ERROR: socket is not open.");
+    }
 }
 
 const btn = document.querySelector("#start")
 btn.addEventListener("click", () => {
-    getMedia().then(() => {
-        setInterval(async () => {
-            const data = await captureFrame();
-            if (data) {
-                sendData(data);
-            } else {
-                console.error("Couldn't capture frame");
-            }
-        }, 1000);
-    });
+    const ws = new WebSocket("ws://127.0.0.1:8080/ws");
+    setInterval(() => {
+        getMedia().then(() => {
+            captureFrame().then((data) => {
+                sendData(ws, data);
+            });
+        });
+    }, 1000);
 });
